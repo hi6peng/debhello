fa80998a415182a1de921f0dec223b17f5ae7815
6peng <hi6peng@qq.com>
2024-04-24 22:00:54 +0800

    lab-2

---
+++ lab-2/debian/debhello.tmpfile.ex
@@ -0,0 +1,3 @@
+#!/bin/sh
+# If this exists, it is installed into usr/lib/tmpfiles.d/debhello.conf in debhello
+# See dh_systemd_enable(1).
---
+++ lab-2/debian/manpage.asciidoc.ex
@@ -0,0 +1,41 @@
+= @UPACKAGE@(1)
+:man source:   DEBHELLO
+:man version:  0.1-1
+:man manual:   DEBHELLO Manual
+
+// See "Manpage Documents" in "AsciiDoc User Guide"
+// http://www.methods.co.nz/asciidoc/userguide.html
+
+// convert by:
+// $ a2x --doctype manpage --format manpage debhello.asciidoc
+
+== NAME
+
+debhello - program to do something
+
+== SYNOPSIS
+
+*debhello*  [*-h*] ... 
+
+
+== DESCRIPTION
+
+*debhello* do something.
+
+=== optional arguments:
+
+*-h*, *--help*::
+    show this help message and exit.
+
+
+== AUTHOR
+
+Copyright © 2024 6peng <hi6peng@qq.com>
+
+== LICENSE
+
+MIT License
+
+== SEE ALSO
+
+See also ...
